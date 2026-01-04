621f7e0c0ac95e01f100483e8ab57933b55bffdd
Emerassi <78173681+Emerassi@users.noreply.github.com>
2026-01-04 13:05:35 -0800

    Update to v13

--- CrystalProjectAPInstaller.cs
+++ CrystalProjectAPInstaller.cs
@@ -105,7 +105,7 @@ class CrystalProjectAPInstaller
 			try
 			{
 				string archipelagoBranchHashString = "9a1e47b7fb5198f86b13279beb9f6f50"; //1.6.5 Archipelago Branch hash
-				string archipelagoModdedVersionString = "cba4ec5352c71bcbc0fee1334b1007e7"; //v0.12.1 Archipelago Modded hash
+				string archipelagoModdedVersionString = "0126175487d0b147105aa82481664a28"; //v0.13.0 Archipelago Modded hash
 
 				//Open crystal project exe path and compute the hash to make sure that it's the right version
 				FileStream crystalProjectBeforeStream = new(crystalProjectExePath, FileMode.Open, FileAccess.Read, FileShare.Read);
